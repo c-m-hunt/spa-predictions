d547b832ba6b75f2b3c7948fc64cedeaa6e05e04
Chris Hunt <chris.hunt@hudl.com>
2020-07-24 21:48:21 +0100

    Update to output table

--- src/app.ts
+++ src/app.ts
@@ -72,4 +72,23 @@ const calculateTable = (data: PredictionData) => {
 
 const points = calculateTable(data);
 
-console.log(points);
+for (const player in points.gamePoints) {
+    console.log("-------------");
+    console.log(player);
+    console.log("-------------");
+    for (const game in points.gamePoints[player]) {
+        console.log(`${game} - ${points.gamePoints[player][game]}`);
+    }
+    console.log("-------------");
+    console.log("             ");
+}
+
+
+const sortedPoints = Array.from(points.leagueTable.entries()).sort((a: any, b: any) => {
+    return a[1] - b[1];
+});
+
+for (const pos in sortedPoints) {
+    const row = sortedPoints[pos][1];
+    console.log(`${parseInt(pos)+1} ${row["name"]} ${row["points"]}`);
+}
